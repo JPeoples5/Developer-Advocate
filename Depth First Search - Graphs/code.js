138be2e1660133372f1844acfbf3f1bbd4dd6dbc
Jeremiah Peoples <48669345+JPeoples5@users.noreply.github.com>
2022-01-14 17:29:05 -1000

    Create code.js
    code solution

---
+++ Depth First Search - Graphs/code.js
@@ -0,0 +1,107 @@
+
+class Graph {
+	constructor() {
+		this.adjacencyList = {}
+	}
+
+	addVertex(vertex) {
+		if (!this.adjacencyList[vertex]) {
+			this.adjacencyList[vertex] = []
+		}
+	}
+
+	addEdge(v1, v2) {
+		this.adjacencyList[v1].push(v2)
+		this.adjacencyList[v2].push(v1)
+	}
+
+	removeEdge(v1, v2) {
+		if (this.adjacencyList[v1].includes(v2)) {
+			this.adjacencyList[v1].splice(this.adjacencyList[v1].indexOf(v2), 1)
+		}
+
+		this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1)
+	}
+
+	removeVertex(vertex) {
+		for (let key in this.adjacencyList) {
+			if (this.adjacencyList[vertex]) delete this.adjacencyList[vertex]
+
+			if (this.adjacencyList[key].includes(vertex)) {
+				this.adjacencyList[key] = this.adjacencyList[key].filter((arrItem) => arrItem !== vertex)
+			}
+		}
+	}
+
+	recursiveDFS(start) {
+		const results = []
+		const visited = {}
+		// the 'this' concept changes with the second dfs func
+		const adjacencyList = this.adjacencyList
+
+		//helper func
+		function dfs(vtx) {
+			if (!vtx) return undefined
+
+			// obj for O(1) lookup
+			visited[vtx] = true
+			results.push(vtx)
+
+			//loop over adjList's key's values
+			for (let key in adjacencyList) {
+				if (!visited[key]) return dfs(key)
+			}
+		}
+		dfs(start)
+		return results
+	}
+
+	iteratvieDFS(start) {
+		const stack = [start] // ex: ['A']
+		const visited = {}
+		const results = []
+		let currentVtx
+
+		visited[start] = true // {'A' : true}
+
+
+		while (stack.length) {
+
+
+			currentVtx = stack.pop() // 'A'
+			results.push(currentVtx) // results = ['A']
+
+			this.adjacencyList[currentVtx].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true
+					stack.push(neighbor)
+				}
+			})
+		}
+
+		return results
+	}
+  
+	BFS(start) {
+		const queue = [start]
+		const visited = {}
+		const results = []
+
+		visited[start] = true
+		let currentVtx
+		while (queue.length) {
+			currentVtx = queue.shift()
+			results.push(currentVtx)
+
+			this.adjacencyList[currentVtx].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true
+					//if unshifted() this becomes DFS
+					queue.push(neighbor)
+				}
+			})
+		}
+
+		return results
+	}
+}
